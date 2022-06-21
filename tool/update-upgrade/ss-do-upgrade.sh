a064b7b534c7305c67b5a2b03dd3f26bb35b9f0d
shenmo <jifengshenmo@outlook.com>
2022-06-21 22:37:39 +0800

    修改：     debian/changelog 	修改：     debian/control 	修改：     debian/spark-store.postinst 	修改：     debian/spark-store.prerm 	修改：     spark-store-project.pro 	修改：     src/main.cpp 	新文件：   tool/apt-fast-conf/apt-fast.conf 	新文件：   tool/ss-apt-fast 	修改：     tool/ssinstall 	修改：     tool/update-upgrade/ss-do-upgrade.sh 	修改：     tool/update-upgrade/ss-update-controler.sh

--- tool/update-upgrade/ss-do-upgrade.sh
+++ tool/update-upgrade/ss-do-upgrade.sh
@@ -5,5 +5,5 @@ echo "没有安装apt-fast，使用apt运行"
 sudo apt upgrade -o Dir::Etc::sourcelist="sources.list.d/sparkstore.list"     -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 else
 echo "已安装apt-fast，使用apt-fast加速运行"
-sudo apt-fast upgrade -o Dir::Etc::sourcelist="sources.list.d/sparkstore.list"     -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+ss-apt-fast upgrade -o Dir::Etc::sourcelist="sources.list.d/sparkstore.list"     -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 fi
