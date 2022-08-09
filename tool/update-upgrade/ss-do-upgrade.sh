b639a9d72654b8200ab591dedca965936ae06e10
shenmo <jifengshenmo@outlook.com>
2022-08-09 10:35:33 +0000

    !72 * 修复使用更新和安装设置更新商店本体时出错 * * 修复使用更新和安装设置更新商店本体时出错

--- tool/update-upgrade/ss-do-upgrade.sh
+++ tool/update-upgrade/ss-do-upgrade.sh
@@ -6,7 +6,7 @@ if [ "$yes_or_no" = "y" ];then
 mkdir -p /tmp/ss-apt-fast-conf/sources.list.d
 
 
-sudo bwrap --dev-bind / / --bind '/opt/durapps/spark-store/bin/apt-fast-conf/sources.list.d/sparkstore.list' /etc/apt/sources.list.d/sparkstore.list ss-apt-fast upgrade -y -o Dir::Etc::sourcelist="sources.list.d/sparkstore.list"     -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
+sudo ss-apt-fast upgrade -y -o Dir::Etc::sourcelist="sources.list.d/sparkstore.list"     -o Dir::Etc::sourceparts="-" -o APT::Get::List-Cleanup="0"
 
 
 else
