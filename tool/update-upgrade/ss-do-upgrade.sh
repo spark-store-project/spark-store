c7ee32a452101ab816b4c965ecd1cb88d286bd0d
shenmo <jifengshenmo@outlook.com>
2022-10-23 10:10:55 +0800

    pkexec 修复

--- tool/update-upgrade/ss-do-upgrade.sh
+++ tool/update-upgrade/ss-do-upgrade.sh
@@ -22,7 +22,7 @@ done | zenity --list --text="选择你想更新的应用" --column=是否更新 
 if [ "$PKG_UPGRADE_LIST" = "" ];then
 zenity --info --icon-name=spark-store --text "没有选中任何软件\n但是你并没有站在世界之巅" --title "星火商店更新检测服务" --height 150 --width 300
 else
-sudo aptss install $PKG_UPGRADE_LIST -y | zenity --progress --auto-close --no-cancel --pulsate --text=正在更新已选中的应用，请稍候... --height 70 --width 400 --title="星火商店更新模块"
+pkexec bash aptss install $PKG_UPGRADE_LIST -y | zenity --progress --auto-close --no-cancel --pulsate --text=正在更新已选中的应用，请稍候... --height 70 --width 400 --title="星火商店更新模块"
 
 if [ "$?" = "0" ];then
 
