369a7801ab110ef752fdd6e6c301fe8130ceaf85
uniartisan <lizhiyuan@uniartisan.com>
2023-03-11 00:38:55 +0800

    fix: 使用cpu核心数目构建，防止小内存机器OOM

--- build_and_install.sh
+++ build_and_install.sh
@@ -18,7 +18,7 @@ echo "sudo apt install git qt5-default debhelper pkg-config qtchooser libqt5core
 echo "Ubuntu 22.04 系统下, 安装依赖"
 echo "sudo apt install git qtbase5-dev debhelper pkg-config qtchooser libqt5core5a libqt5gui5 libqt5widgets5 libqt5network5 libqt5concurrent5 libdtkcore-dev libdtkgui-dev  libdtkwidget-dev qttools5-private-dev libnotify-dev qtwebengine5-dev qtwayland5 qtwayland5-dev-tools"
 
-dpkg-buildpackage -j
+dpkg-buildpackage -j$(cat /proc/cpuinfo | grep processor | wc -l)
 sudo apt reinstall ../spark-store_*.deb
 
 rm ../spark-store_*
