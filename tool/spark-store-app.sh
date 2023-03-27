df6b2f268a0db70f85d7a494f07610606c45d55d
uniartisan <lizhiyuan@uniartisan.com>
2023-03-27 16:17:02 +0800

    fix: ld path

--- tool/spark-store-app.sh
+++ tool/spark-store-app.sh
@@ -4,7 +4,7 @@ SELF=$(readlink -f "$0")
 HERE=${SELF%/*}
 
 export PATH="${HERE}/bin${PATH:+:$PATH}"
-export LD_LIBRARY_PATH="${HERE}/lddpath${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+export LD_LIBRARY_PATH="${HERE}/lddpath${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH:/usr:/usr/local:/lib/x86_64-linux-gnu}"
 export QT_PLUGIN_PATH="${HERE}/plugins"
 
 exec spark-store $*
