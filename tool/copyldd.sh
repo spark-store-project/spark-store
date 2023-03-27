0fb719e11c4de2b4230a7763edfb8b3f027584ee
uniartisan <lizhiyuan@uniartisan.com>
2023-03-27 17:43:51 +0800

    fix: appimage

--- tool/copyldd.sh
+++ tool/copyldd.sh
@@ -5,22 +5,24 @@ script_dir=$(cd $(dirname $0);pwd)
 
 CURDIR=$(dirname $script_dir)
 
-mkdir -p $CURDIR/debian/spark-store/opt/durapps/spark-store/lddpath
+mkdir -p $CURDIR/debian/spark-store/opt/durapps/spark-store/lib
 so_list=$(ldd $CURDIR/debian/spark-store/opt/durapps/spark-store/bin/spark-store | awk '{if (match($3, "/")){ print $3}}')
 
-cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lddpath
+cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lib
 
 # so_list=$(ls /lib/x86_64-linux-gnu/libQt5*.so)
-# cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lddpath
+# cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lib
 
 
-mkdir -p $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins
-cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
-cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/wayland-shell-integration $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
-cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/iconengines $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
-cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/imageformats $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
-cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/platformthemes $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
-cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/styles $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
+# mkdir -p $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins
+# cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
+# cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/wayland-shell-integration $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
+# cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/iconengines $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
+# cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/imageformats $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
+# cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/platformthemes $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
+# cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/styles $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
 
 
 # cp -r /usr/share/libdtk*/ $CURDIR/debian/spark-store/opt/durapps/spark-store/ >/dev/null || :
+$CURDIR/linuxdeployqt $CURDIR/debian/spark-store/opt/durapps/spark-store/bin/spark-store -unsupported-allow-new-glibc -appimage
+rm -rf $CURDIR/debian/spark-store/opt/durapps/AppRun
--- tool/spark-store-app.sh
+++ tool/spark-store-app.sh
@@ -3,8 +3,8 @@
 SELF=$(readlink -f "$0")
 HERE=${SELF%/*}
 
-export PATH="${HERE}/bin${PATH:+:$PATH}"
-export LD_LIBRARY_PATH="${HERE}/lddpath${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH:/usr:/usr/local:/lib/x86_64-linux-gnu}"
-export QT_PLUGIN_PATH="${HERE}/plugins"
+export PATH="/opt/durapps/spark-store/bin${PATH:+:$PATH}"
+export LD_LIBRARY_PATH="/opt/durapps/spark-store/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH:/usr:/usr/local:/lib/x86_64-linux-gnu}"
+# export QT_PLUGIN_PATH="${HERE}/plugins"
 
 exec spark-store $*
