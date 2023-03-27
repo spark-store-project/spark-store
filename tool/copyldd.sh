b6f3d2a881d5634f0e59287c4c96a00a322dd3f6
uniartisan <lizhiyuan@uniartisan.com>
2023-03-27 19:48:33 +0800

    fix

--- tool/copyldd.sh
+++ tool/copyldd.sh
@@ -22,6 +22,16 @@ cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/imageformats $CURDIR/debian/spark
 cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/platformthemes $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
 cp -L -r /usr/lib/x86_64-linux-gnu/qt5/plugins/styles $CURDIR/debian/spark-store/opt/durapps/spark-store/plugins >/dev/null || :
 
+so_list=$(ldd /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms/libqxcb.so | awk '{if (match($3, "/")){ print $3}}')
+cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lib
+so_list=$(ldd /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms/libqwayland-generic.so | awk '{if (match($3, "/")){ print $3}}')
+cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lib
+so_list=$(ldd /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms/libqwayland-egl.so  | awk '{if (match($3, "/")){ print $3}}')
+cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lib
+so_list=$(ldd /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms/libqwayland-xcomposite-egl.so | awk '{if (match($3, "/")){ print $3}}')
+cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lib
+so_list=$(ldd /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms/libqwayland-xcomposite-glx.so | awk '{if (match($3, "/")){ print $3}}')
+cp -L -n $so_list $CURDIR/debian/spark-store/opt/durapps/spark-store/lib
 
 # cp -r /usr/share/libdtk*/ $CURDIR/debian/spark-store/opt/durapps/spark-store/ >/dev/null || :
 $CURDIR/linuxdeployqt $CURDIR/debian/spark-store/opt/durapps/spark-store/bin/spark-store -appimage -bundle-non-qt-libs >/dev/null || :
