8dc1854f2e5017c061941d88a7a0c9d280e9c45d
shenmo <jifengshenmo@outlook.com>
2024-11-14 11:54:33 +0000

    update tool/update-upgrade/ss-do-upgrade.sh.
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- tool/update-upgrade/ss-do-upgrade.sh
+++ tool/update-upgrade/ss-do-upgrade.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-xhost +
 if [ "$(id -u)" != "0" ] ; then
     if [ "$IS_ACE_ENV" = "1" ];then
         /opt/durapps/spark-store/bin/store-helper/pass-auth.sh "$0" "$@"
     else
+       xhost +
 	   pkexec "$0" "$@"
 	   exit
     fi
