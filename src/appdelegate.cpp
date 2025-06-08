705478fdf0c3157543bb0afca540527e550d762b
momen <vmomenv@gmail.com>
2025-06-08 10:33:34 +0800

    chore:取消更新信息的显示

--- src/appdelegate.cpp
+++ src/appdelegate.cpp
@@ -61,7 +61,8 @@ void AppDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, c
     painter->setFont(normalFont);
     painter->setPen(QColor("#AAAAAA"));
     painter->drawText(descRect, Qt::TextWordWrap,
-                      QString("更新说明：%1\n包大小：%2").arg(description, size));
+                    //   QString("更新说明：%1\n包大小：%2").arg(description, size));
+                    QString("包大小：%1").arg(size));
 
     // 更新按钮（样式占位）
     QRect buttonRect(rect.right() - 80, rect.top() + (rect.height() - 30) / 2, 70, 30);
