e69deb4b95f6f56276bd1e584c44bf2e4ef1a726
momen <vmomenv@gmail.com>
2025-07-04 04:08:09 +0800

    将更新器合入到星火商店中

---
+++ spark-update-tool/src/mainwindow.h
@@ -0,0 +1,33 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "aptssupdater.h"
+#include "applistmodel.h"
+#include "appdelegate.h"
+#include <QListView>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MainWindow;
+}
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    void checkUpdates();
+    void initStyle();
+    void runAptssUpgrade();
+    AppListModel *m_model;
+    AppDelegate *m_delegate;
+    QListView *listView; // 声明 QListView 指针
+};
+#endif // MAINWINDOW_H
