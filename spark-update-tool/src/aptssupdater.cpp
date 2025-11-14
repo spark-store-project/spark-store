36ec2a4e8bff499f97dca1946a0330083c352bcc
shenmo <jifengshenmo@outlook.com>
2025-11-14 02:15:11 +0000

    update spark-update-tool/src/aptssupdater.cpp.
    Signed-off-by: shenmo <jifengshenmo@outlook.com>

--- spark-update-tool/src/aptssupdater.cpp
+++ spark-update-tool/src/aptssupdater.cpp
@@ -81,7 +81,7 @@ QStringList aptssUpdater::getPackageSizes()
         
         // 构建新命令（包含包名参数）
         QString command = QString("/usr/bin/apt download %1 --print-uris -c /opt/durapps/spark-store/bin/apt-fast-conf/aptss-apt.conf "
-                                  "-o Dir::Etc::sourcelist=\"/opt/durapps/spark-store/bin/apt-fast-conf/sources.list.d/sparkstore.list\" "
+                                  "-o Dir::Etc::sourcelist=\"/opt/durapps/spark-store/bin/apt-fast-conf/sources.list.d/aptss.list\" "
                                   "-o Dir::Etc::sourceparts=\"/dev/null\"").arg(packageName);
 
         process.start("bash", QStringList() << "-c" << command);
